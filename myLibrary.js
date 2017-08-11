fe43110f9e8b52d36959d6c0403c6ed0c3b315ca
Anton Parayno <anton.parayno@gmail.com>
2017-08-11 08:20:34 -0400

    edited myLibrary.js

--- myLibrary.js
+++ myLibrary.js
@@ -3,22 +3,14 @@
 
 	function librarySystem(libraryName, array, callback){
 		if(arguments.length > 1){
-			if(array.length > 0){
-				var libraryArray = [];
-				for(var i = 0; i < array.length; i++){
-					if(i in array){
-					var libName = libraryStorage[array[i]];
-					libraryArray.push(libName);
-					}
-				};
-				libraryStorage[libraryName] = callback.apply(this, libraryArray);
-			} else{
-				libraryStorage[libraryName] = callback()
-			};
+			var libraryArray = array.map(function(libName){
+				return libraryStorage[libName];
+			}).filter(function(element,i){
+				return i in array;
+			});
 		} else {
 			return libraryStorage[libraryName];
 		};	
 	};
-
 	window.librarySystem = librarySystem;
 })();
