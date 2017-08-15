b881b7add0a98d9e301ccc852b5f77e0c328e0e4
Anton Parayno <anton.parayno@gmail.com>
2017-08-15 18:02:07 -0400

    refactored myLibrary.js and improvedRunwithDebugger.html

--- myLibrary.js
+++ myLibrary.js
@@ -1,12 +1,10 @@
 (function(){
 	var libraryStorage = {};
 
-	function librarySystem(libraryName, array, callback){
-		if(arguments.length > 1){
-			var libraryArray = array.map(function(libName){
+	function librarySystem(libraryName, dependencyArray, callback){
+		if(dependencyArray){
+			var libraryArray = dependencyArray.map(function(libName){
 				return libraryStorage[libName];
-			}).filter(function(element,i){
-				return i in array;
 			});
 			libraryStorage[libraryName] = callback.apply(this, libraryArray);
 		} else {
